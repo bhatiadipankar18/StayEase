a5e1472d25c5b7756f47fac98c7bba34e0294a46
Dipankar Bhatia <dipankar@Dipankars-MacBook-Air.local>
2025-01-19 00:03:18 -0500

    first commit

---
+++ src/test/java/com/application/StayEase/StayEaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.StayEase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StayEaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
